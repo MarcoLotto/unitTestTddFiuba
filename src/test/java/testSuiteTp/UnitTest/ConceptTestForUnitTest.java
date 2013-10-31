eed0ea88a267fcb1dcd778c63d07879ece8d25a4
Marco Lotto <marcol91@gmail.com>
2013-10-31 13:15:53 -0300

    Se agrega un test unitario jUnit que prueba funcionamiento de los UnitTest

---
+++ src/test/java/testSuiteTp/UnitTest/ConceptTestForUnitTest.java
@@ -0,0 +1,15 @@
+package testSuiteTp.UnitTest;
+
+import org.junit.Test;
+
+import testSuiteTp.TestInterface.TestSuite;
+
+public class ConceptTestForUnitTest {
+
+	@Test
+	public void conceptUnitTest() {
+		TestSuite suite = new TestSuiteForTesting("TestSuiteForTesting");
+		suite.runAllTests();
+	}
+
+}
--- src/test/java/testSuiteTp/UnitTest/TestingUnitTest.java
+++ src/test/java/testSuiteTp/UnitTest/TestingUnitTest.java
@@ -1,15 +1,33 @@
 package testSuiteTp.UnitTest;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import testSuiteTp.TestInterface.TestSuite;
+import testSuiteTp.TestInterface.UnitTest;
 
 public class TestingUnitTest {
 
 	@Test
-	public void conceptUnitTest() { 
-		TestSuite suite = new TestSuiteForTesting("TestSuiteForTesting");
-		suite.runAllTests();
+	public void testMustPass1() {
+		UnitTest test = new UnitTestForTesting1("Test1");
+		test.run();
+		assertTrue(test.hasPassed());
+	}
+
+	@Test
+	public void testMustPass2() {
+		UnitTest test = new UnitTestForTesting3("Test3");
+		test.run();
+		assertTrue(test.hasPassed());
+	}
+
+	@Test
+	public void testMustFail() {
+		UnitTest test = new UnitTestForTesting2("Test2");
+		test.run();
+		assertFalse(test.hasPassed());
 	}
 
 }
