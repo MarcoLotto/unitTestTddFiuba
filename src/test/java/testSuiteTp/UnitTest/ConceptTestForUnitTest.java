5fe911f4742a4bda51c90d67cbd903e0f09a87dd
TomR <tomasreale@gmail.com>
2013-11-07 06:57:59 -0300

    solucionando conflictos de merge

--- src/test/java/testSuiteTp/UnitTest/ConceptTestForUnitTest.java
+++ src/test/java/testSuiteTp/UnitTest/ConceptTestForUnitTest.java
@@ -8,13 +8,8 @@ public class ConceptTestForUnitTest {
 
 	@Test
 	public void conceptUnitTest() {
-<<<<<<< HEAD
-		TestSuite suite = new TestSuiteForTesting("TestSuiteForTesting");
-		suite.run(".*1");
-=======
 		TestSuite suite = new TestSuiteForTesting1("TestSuiteForTesting1");
 		suite.run();
->>>>>>> 67652e6d1ced6ab5a307da53e5726d0b26f75187
 	}
 
 }
